bbd3cdada919689742727cce5d62e682d151cbbb
kz-yamamoto <takydromus.dorsalis@gmail.com>
2023-04-17 15:21:14 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,44 @@
+# http://tinyurl.com/h9q2cpc
+
+
+def hangman(word):
+    wrong = 0
+    stages = [
+        "",
+        "-----",
+        "|              ",
+        "|      |       ",
+        "|      o       ",
+        "|     /|\      ",
+        "|     / \      ",
+        "|              ",
+    ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("welcome to hangman!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print(e)
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("you win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0 : wrong + 1]))
+        print("you lose! 正解は{}".format(word))
+
+
+hangman("cat")
